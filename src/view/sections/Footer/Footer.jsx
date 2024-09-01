a7614b87362059c497a311d5fca611b7d75982e5
Kamil Donda <dondakamil@gmail.com>
2024-09-01 11:18:23 +0200

    Send IP of client

--- src/view/sections/Footer/Footer.jsx
+++ src/view/sections/Footer/Footer.jsx
@@ -1,7 +1,24 @@
 import "./Footer.css";
+import React, { useEffect, useState } from "react";
 
 export function Footer() {
+  const [ipAddress, setIPAddress] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.ipify.org?format=json")
+      .then((response) => response.json())
+      .then((data) => setIPAddress(data.ip))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <footer>Copyright &copy; 2023 Kamil Donda. All Rights Reserved<img id="footer-icon" src="https://frog01-21353.wykr.es/portfolio-icon" alt="Goodbye!" /></footer>
+    <footer>
+      Copyright &copy; 2023 Kamil Donda. All Rights Reserved
+      <img
+        id="footer-icon"
+        src={`https://frog01-21353.wykr.es/portfolio-icon?ipAddress=${ipAddress}`}
+        alt="Goodbye!"
+      />
+    </footer>
   );
 }
